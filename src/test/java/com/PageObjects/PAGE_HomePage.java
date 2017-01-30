0ae22d2c0d381fb0ff28087a5c036bba22666a7e
Raj <rjsekhar007@gmail.com>
2017-01-29 20:48:46 -0500

    Changed

--- src/test/java/com/PageObjects/PAGE_HomePage.java
+++ src/test/java/com/PageObjects/PAGE_HomePage.java
@@ -1,20 +1,29 @@
 package com.PageObjects;
 
+import com.Setup.Constants;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import java.util.List;
-
 public class PAGE_HomePage extends PAGE_Base {
     @Test
-    public void checkHomePage()
+    public void checkHomePageLayout()
     {
-        List<WebElement> list=driver.findElements(By.tagName("*"));
+       WebElement tutorials= driver.findElement(By.partialLinkText(Constants.homepage_tutorials_pLT));
+       WebElement references=driver.findElement(By.partialLinkText(Constants.homepage_references_plT));
+       WebElement examples=driver.findElement(By.partialLinkText(Constants.homepage_examples_pLT));
+       WebElement globe=driver.findElement(By.xpath(Constants.homepage_globe_xpath));
+        WebElement search=driver.findElement(By.xpath(Constants.homepage_search_xpath));
 
-        for(WebElement temp:list)
+        if(tutorials.isDisplayed()&&references.isDisplayed() && examples.isDisplayed() && globe.isDisplayed() && search.isDisplayed())
         {
-            System.out.println("TagName:"+temp.getTagName()+" Text:"+temp.getText());
+            System.out.println("w3schools home page is displayed");
+            assert true;
+        }
+        else
+        {
+            System.out.println("One of the element in the home page is not displayed");
+            assert false;
         }
     }
 }
--- src/test/java/com/Setup/Constants.java
+++ src/test/java/com/Setup/Constants.java
@@ -5,4 +5,9 @@ package com.Setup;
  */
 public class Constants {
     public static String baseUrl="http://www.w3schools.com/";
+    public static String homepage_tutorials_pLT="TUTORIALS";
+    public static String homepage_references_plT="REFERENCES";
+    public static String homepage_examples_pLT="EXAMPLES";
+    public static String homepage_globe_xpath="//*/ul/li[6]/a";
+    public static String homepage_search_xpath="//*/ul/li[5]/a";
 }
